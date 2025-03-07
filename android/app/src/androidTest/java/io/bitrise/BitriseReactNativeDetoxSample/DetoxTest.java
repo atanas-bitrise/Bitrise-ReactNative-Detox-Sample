0919d126c28a918d161175e22c532120bb54532e
Atanas Chanev <atanas.chanev@bitrise.io>
2025-03-07 16:52:43 +0000

    Remove deprecated reference to detox config

--- android/app/src/androidTest/java/io/bitrise/BitriseReactNativeDetoxSample/DetoxTest.java
+++ android/app/src/androidTest/java/io/bitrise/BitriseReactNativeDetoxSample/DetoxTest.java
@@ -1,7 +1,6 @@
 package io.bitrise.BitriseReactNativeDetoxSample;
 
 import com.wix.detox.Detox;
-import com.wix.detox.config.DetoxConfig;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,11 +18,6 @@ public class DetoxTest {
 
     @Test
     public void runDetoxTests() {
-        DetoxConfig detoxConfig = new DetoxConfig();
-        detoxConfig.idlePolicyConfig.masterTimeoutSec = 90;
-        detoxConfig.idlePolicyConfig.idleResourceTimeoutSec = 60;
-        detoxConfig.rnContextLoadTimeoutSec = (BuildConfig.DEBUG ? 180 : 60);
-
         Detox.runTests(mActivityRule, detoxConfig);
     }
 }
